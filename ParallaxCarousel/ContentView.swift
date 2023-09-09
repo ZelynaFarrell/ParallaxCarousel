5fdc31d9bccfa0d0f128b8a94b2d0f72ec58781c
Zelyna Farrell <zsillas@yahoo.com>
2023-09-09 13:45:02 -0700

    Initial Commit

---
+++ ParallaxCarousel/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ParallaxCarousel
+//
+//  Created by Zelyna Sillas on 9/9/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ParallaxCarousel/ParallaxCarouselApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParallaxCarouselApp.swift
+//  ParallaxCarousel
+//
+//  Created by Zelyna Sillas on 9/9/23.
+//
+
+import SwiftUI
+
+@main
+struct ParallaxCarouselApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
